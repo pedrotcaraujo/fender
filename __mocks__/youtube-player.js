ee85f6fb65d580d08a205e422e1970a079096a27
Pedro Araujo <pedrotcaraujo@gmail.com>
2017-12-17 23:57:05 -0200

    Updates

--- __mocks__/youtube-player.js
+++
@@ -1,7 +0,0 @@
-const youtubePlayer = jest.fn(() => ({loadVideoById, on, getCurrentTime}));
-
-const loadVideoById = jest.fn();
-const on = jest.fn();
-const getCurrentTime = jest.fn(() => Promise.resolve(10))
-
-module.exports = youtubePlayer;
--- src/components/Players/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Youtube } from './Youtube';
